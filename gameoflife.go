bd2882756be8efcef229cc41e6b9c0d0488c99eb
Samuel Bisciglia <sbiscigl@Samuels-MacBook-Pro.local>
2017-04-24 19:57:39 -0500

    pass in width and height as params

--- gameoflife.go
+++ gameoflife.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/sbiscigl/conways/board"
 	"github.com/sbiscigl/conways/game"
 	"github.com/sbiscigl/conways/rules"
 )
 
 func main() {
-	b := board.New(40, 140)
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ")
+		fmt.Println("conways <Width> <Height>")
+	}
+	w, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(fmt.Sprintf("width not number"))
+	}
+	h, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		panic(fmt.Sprintf("height not number"))
+	}
+	b := board.New(w, h)
 	b.Init()
 	game.New(b, rules.New()).Tick()
 }
